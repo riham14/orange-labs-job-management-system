16c8115787f966632b00d6734dd99b1aa8ac5617
Riham Nour <riham.nour@nasnav.com>
2019-05-25 15:43:49 +0200

    Initial commit

---
+++ src/main/java/com/orange/job/management/OrangeJobManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.orange.job.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeJobManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeJobManagementSystemApplication.class, args);
+    }
+
+}
